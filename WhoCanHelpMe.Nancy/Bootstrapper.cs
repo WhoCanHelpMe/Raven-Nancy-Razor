10d4f588f4edb53aa195789aab62d906baa087a1
Jonathan George <jon@jonathangeorge.co.uk>
2014-09-19 15:36:36 +0100

    Initial commit

---
+++ WhoCanHelpMe.Nancy/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿namespace WhoCanHelpMe.Nancy
+{
+    using global::Nancy;
+    using global::Nancy.Conventions;
+
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/css"));
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/images"));
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/js"));
+
+            base.ConfigureConventions(nancyConventions);
+        }
+    }
+}
---
+++ WhoCanHelpMe.Nancy/Modules/Home/HomeModule.cs
@@ -0,0 +1,17 @@
+﻿ namespace WhoCanHelpMe.Nancy.Modules.Home
+{
+    using global::Nancy;
+
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            this.Get["/"] = GetHome;
+        }
+
+        private object GetHome(dynamic arg)
+        {
+            return this.View["Home"];
+        }
+    }
+}
